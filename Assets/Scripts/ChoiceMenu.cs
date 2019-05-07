04cb74708af9b74270f94636a71430f7e7fb7ed0
Florent Vain <florent.vain@makorj.fr>
2019-05-07 15:43:08 +0200

    Added choice menu, still have to make all cards and trigger the right upgrade on choice

---
+++ Assets/Scripts/ChoiceMenu.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChoiceMenu : MonoBehaviour
+{
+    static private ChoiceMenu instance = null;
+
+    public UpgradeCard[] cards = new UpgradeCard[3];
+
+    private int activeCardIndex;
+
+    public float cardSpeed = 1;
+    private bool moving = false;
+    private void Awake() {
+        if(instance == null) instance = this; else Destroy(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0;
+        activeCardIndex = 1;
+        moving = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(moving)
+            return;
+
+        float value = Input.GetAxis("Horizontal");
+
+        if(value > 0 || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if(activeCardIndex!=0)
+            {
+                activeCardIndex--;
+
+                foreach (UpgradeCard card in cards)
+                {
+                    card.targetPosition = card.transform.position - (Vector3.right * 5f);
+                }
+                StartCoroutine("MoveCards");
+                moving = true;
+            }
+        }
+        else if(value < 0  || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if(activeCardIndex!=2)
+            {
+                activeCardIndex++;
+                foreach (UpgradeCard card in cards)
+                {
+                    card.targetPosition = card.transform.position + (Vector3.right * 5f);
+                }
+                StartCoroutine("MoveCards");
+
+                moving = true;
+            }
+        }
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+    IEnumerator MoveCards()
+    {
+        while(cards[0].gameObject.transform.position != cards[0].targetPosition)
+        {
+            foreach (UpgradeCard card in cards)
+            {
+                card.gameObject.transform.position = Vector3.MoveTowards(card.transform.position, card.targetPosition, cardSpeed);
+            }
+            yield return null;
+        }
+        moving =false;
+        Time.timeScale = 1;
+    }
+
+    static public void SetUpgradeCards(Stat.Type card1, Stat.Type card2, Stat.Type card3)
+    {
+        instance.gameObject.SetActive(true);
+        instance.cards[0].Set(card1);
+        instance.cards[1].Set(card2);
+        instance.cards[2].Set(card3);
+    }
+
+    static public void SetUpgradeCards(AMJ.GunUpgrade gunCard, AMJ.SwordUpgrade swordCard, AMJ.OtherUpgrade otherCard)
+    {
+        instance.gameObject.SetActive(true);
+        instance.cards[0].Set(gunCard);
+        instance.cards[1].Set(swordCard);
+        instance.cards[2].Set(otherCard);
+
+    }
+}
---
+++ Assets/Scripts/UpgradeCard.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeCard : MonoBehaviour
+{
+    public Vector3 targetPosition;
+
+    public Sprite[] spritesList;
+
+    public TMPro.TMP_Text title;
+    public TMPro.TMP_Text text;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPosition = new Vector3();
+    }
+
+    public void Set(AMJ.GunUpgrade upgrade)
+    {
+        switch (upgrade)
+        {
+            case AMJ.GunUpgrade.Autoguide:
+                title.text = "Self-guided bullet";
+                text.text = "Bend projectile trajectory to the closest target";
+                break;
+            case AMJ.GunUpgrade.Bouncing:
+                title.text = "Bouncing laser";
+                text.text = "Projectile will never be lost again in space, instead they will bounce and go back to th asteroïd field";
+                break;
+            case AMJ.GunUpgrade.Charging:
+                title.text = "Charging gun";
+                text.text = "Charge your shot over time to increase all your effects !";
+                break;
+            case AMJ.GunUpgrade.Damage:
+                title.text = "Damage ++";
+                text.text = "Double all the damages dealt with the gun, forever";
+                break;
+            case AMJ.GunUpgrade.Fire:
+                title.text = "Fire upgrade";
+                text.text = "Every projectile that hit an asteroïd will burn it to piece, dealing damage over time";
+                break;
+            case AMJ.GunUpgrade.Ice:
+                title.text = "Ice upgrade";
+                text.text = "Projectile frost asteroïds to slow them down, you'll be able to deal way more damage";
+                break;
+            case AMJ.GunUpgrade.Multishot:
+                title.text = "Multishot";
+                text.text = "Fire 3 projectiles instead of one !";
+                break;
+            case AMJ.GunUpgrade.Piercing:
+                title.text = "Piercing laser";
+                text.text = "Every laser shot go through enemies";
+                break;
+            case AMJ.GunUpgrade.Thunder:
+                title.text = "Thunder upgrade";
+                text.text = "With the power of electricity, do something strong as always";
+                break;
+        }
+    }
+
+    public void Set(AMJ.OtherUpgrade upgrade)
+    {
+
+    }
+
+    public void Set(AMJ.SwordUpgrade upgrade)
+    {
+
+    }
+
+    public void Set(Stat.Type upgrade)
+    {
+
+    }
+}
